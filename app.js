25b57bc7503a79db0b63513920a79852eb30614c
Alena Aparina <yelenaaparina@yandex.ru>
2022-01-30 11:55:03 +0300

    feat: логирование и CORS

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
@@ -7,6 +9,7 @@ const { login, createUser } = require('./controllers/users');
 const { userValidation, loginValidation } = require('./middlewares/validation');
 const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/NotFoundError');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -15,6 +18,29 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+const corsConfig = {
+  origin: [
+    'http://localhost:3000',
+    'http://mestofront.aparinalena.nomoredomains.work',
+    'https://mestofront.aparinalena.nomoredomains.work',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
+
+app.use('*', cors(corsConfig));
+
+app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.post('/signin', loginValidation, login);
 app.post('/signup', userValidation, createUser);
@@ -29,6 +55,7 @@ app.use((req, res, next) => {
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {});
 
+app.use(errorLogger);
 app.use(errors());
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,11 +2,14 @@ const jwt = require('jsonwebtoken');
 const AuthError = require('../errors/AuthError');
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
+  const { NODE_ENV, JWT_SECRET } = process.env;
   let payload;
 
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(
+      req.cookies.token,
+      NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+    );
   } catch (err) {
     throw new AuthError('Необходимо авторизоваться');
   }
